d4630400e0f4d416091588aeb9405567b1ecd13c
Luan Rodrigues <luanrodrigues007@hotmail.com>
2022-01-28 19:40:35 -0300

    Documentacao cancer

--- src/estrategias/cancer.py
+++ src/estrategias/cancer.py
@@ -1,19 +1,32 @@
 def _get_cidade(gene):
+    """
+    Função auxiliar para pegar o No (cidade) do gene.
+
+    :param gene: Gene com a aresta.
+    :return: No cidade do gene.
+    """
     return gene.alelo.no_destino
 
 
-def _achar_cidade(cidades, procurada):
-    for cidade in cidades:
-        if cidade == procurada:
-            return cidade
-
-
-TEM_CANCER = True
-NAO_TEM_CANCER = False
-
-
 class VerificarCancerEstrategy:
+    """
+    Estratégia para validar 1 cromosomo e verificar se
+    o caminho percorrido é válido.
+    """
+
     def tem_cancer(self, comossomo):
+        """
+        Verifica se o cromossomo é váilido. Para ser válido, deve atender
+        a 2 requisitos:
+
+        1 - Não pode haver repetíção de cidades
+        2 - Deve ser possível andar pelo mapa seguindo a ordem das arestas
+            presente nos genes do cromossomo.
+
+        :param comossomo: Cromossomo a ser validado
+        :return: String contendo o motivo do cancer, ou uma string vazia
+            se não for encontrado cancer.
+        """
         genes_validos = list(filter(bool, comossomo.genes))
         todas_cidades = set(map(_get_cidade, genes_validos))
 
