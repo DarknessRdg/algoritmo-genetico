0130ff08e0b5c8405ba472e16454808e40902cb3
Luan Rodrigues <luanrodrigues007@hotmail.com>
2022-01-28 20:32:21 -0300

    Documentacao cromossomo

--- src/cromossomo.py
+++ src/cromossomo.py
@@ -3,18 +3,19 @@ from typing import Any
 
 
 class Cromossomo:
+    """
+    Classe que serve como estrutura de dados para salvar os dados do cromossomo:
+    - geracao
+    - genes
+    - fitness
+    - cancerigeno
+    """
     def __init__(self, geracao, *genes, cancerigeno=False):
         self.geracao = geracao
         self.genes = genes
 
         self.fitness = None
         self.cancerigeno = cancerigeno
-
-    def primeiros_genes(self, n):
-        return self.genes[:n]
-
-    def ultimos_genes(self, n):
-        return self.genes[n:]
 
     def __str__(self):
         return f'Cromossomo(geracao={self.geracao}, ' \
@@ -30,8 +31,14 @@ class Cromossomo:
     def __eq__(self, other):
         return isinstance(other, Cromossomo) and other.genes == self.genes
 
+
 @dataclass
 class Gene:
+    """
+    Classe para ser estrutura de dados dos genes dos cromossomos, onde o valor
+    do genes pode ser qualquer coisa.
+    - Alelo: valor do gene.
+    """
     alelo: Any
 
     def __repr__(self):
