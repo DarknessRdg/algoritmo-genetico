3b86bce6dfec88b96252068a145cfe5a3afd01fc
Luan Rodrigues <luanrodrigues007@hotmail.com>
2022-01-28 19:35:25 -0300

    Documentacao fintess

--- src/estrategias/fitness.py
+++ src/estrategias/fitness.py
@@ -1,8 +1,24 @@
 class FitnessStrategy:
+    """
+    Estratégia para criar o fitness de 1 cromossomo.
+    """
 
     def calcular(self, genes):
+        """
+        Calula o fitness de 1 cromossomo para serem ordenados. O fitness
+        atende a dois critérios, de modo que os melhores cromossomos tenham:
+
+        1. Maior número de cidades visitadas
+        2. Menor distância percorrida
+
+        :param genes: Tupe[Genes] genes do cromossomo
+        :return: Tupe[Int, Int] - quantidade de cidades, menor distância
+            respectivamente
+        """
         alelos = set([gene.alelo for gene in genes if gene.alelo])
 
         custo = sum(map(lambda alelo: alelo.peso, alelos))
 
+        # custo negativo para que o melhor cromossomo seja o de menor custo.
         return len(alelos), -custo
+
